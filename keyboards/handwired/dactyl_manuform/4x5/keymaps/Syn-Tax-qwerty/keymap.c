8983c2fbeeab696a298d3f3ed9272421573d0842
Oscar Morris <twocap06@gmail.com>
2020-10-13 16:51:47 +0100

    added qwerty

--- keyboards/handwired/dactyl_manuform/4x5/keymaps/Syn-Tax-qwerty/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/Syn-Tax-qwerty/keymap.c
@@ -32,11 +32,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Base (dvorak)
  * ,----------------------------------,                             ,----------------------------------,
- * |   q  |   w  |   e  |   r  |   t  |                             |   y  |   u  |   i  |   o  |   p  |
+ * |   '  |   ,  |   .  |   p  |   y  |                             |   f  |   g  |   c  |   r  |   l  |
  * |------+------+------+------+------|                             |-------------+------+------+------|
- * |   a  |   s  |   d  |   f  |   g  |                             |   h  |   j  |   k  |   l  |   ;  |
+ * |   a  |   o  |   e  |   u  |   i  |                             |   d  |   h  |   t  |   n  |   s  |
  * |------+------+------+------+------|                             |------|------+------+------+------|
- * |   z  |   x  |   c  |   v  |   b  |                             |   n  |   m  |   ,  |   .  |   /  |
+ * |   ;  |   q  |   j  |   k  |   x  |                             |   b  |   m  |   w  |   v  |   z  |
  * |------+------+------+-------------,                             ,-------------+------+------+------,
  *        |  [   |   ]  |                                                         |   (  |   )  |
  *        '------+------'-------------'                             '-------------'------+------'
@@ -52,9 +52,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_BASE] = LAYOUT( \
-  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                       KC_F,   KC_G,    KC_C,    KC_R,   KC_L,    \
-  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                       KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, \
-  KC_Z, KC_X,    KC_C,    KC_V,    KC_B,                                       KC_N,   KC_M,    KC_COMM,    KC_DOT,   KC_SLSH, \
+  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                                       KC_F, KC_G,    KC_C,    KC_R,   KC_L,    \
+  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                                       KC_D, KC_H,    KC_T,    KC_N,   KC_S, \
+  KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,                                       KC_B, KC_M,    KC_W,    KC_V,   KC_Z, \
               KC_LBRC, KC_RBRC,                                                   KC_LPRN, KC_RPRN,                   \
                                       ALT_ESC, KC_BSPC,   KC_SPC,  KC_ENT,                                          \
                                       KC_LSFT, KC_LGUI  ,   RAISE, LOWER,                                           \
